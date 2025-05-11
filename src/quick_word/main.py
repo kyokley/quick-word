c7ed4acae2b4a80f7da8df0f6147edd6ef2f1c5f
Kevin Yokley <kyokley@mars>
2025-05-11 17:58:50 -0500

    Convert docopt to argparse

--- src/quick_word/main.py
+++ src/quick_word/main.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''quick_word
-
-Usage:
-  quick_word <word_count>
-  quick_word -h | --help
-  quick_word --version
-
-Options:
-  -h --help     Show this screen.
-  --version     Show version.
-'''
-
-from __future__ import unicode_literals, print_function
+import argparse
 import sys
-from docopt import docopt
 from .words import get_word
 
 __version__ = "0.1.0"
@@ -22,14 +9,27 @@ __author__ = "Kevin Yokley"
 __license__ = "MIT"
 
 
+parser = argparse.ArgumentParser(
+        prog='quick-word',
+        description='Dump list of random dictionary words',
+        )
+parser.add_argument('--version', action='store_true')
+parser.add_argument('word_count', description='Number of words to generate')
+
+args = parser.parse_args()
+
+
 def main():
     '''Main entry point for the quick_word CLI.'''
-    args = docopt(__doc__, version=__version__)
+    version = args.version
+    if version:
+        print(f' quick-word {__version__}')
+        return
 
     try:
-        word_count = int(args['<word_count>'])
+        word_count = int(args.word_count)
     except ValueError:
-        print(f"Expected an int. Got '{args['<word_count>']}'")
+        print(f"Expected an int. Got '{args.word_count}'")
         sys.exit(1)
     print('\n'.join(get_word() for i in range(word_count)))
 
